758320e9087145b44e5632578b62c070f817ee49
mzaric1998 <zaric.in22.2018@uns.ac.rs>
2021-03-30 15:55:55 +0200

    komit

--- ProjekatSIMS/ProjekatSIMS/Model/UpravnikModel/Prostorija.cs
+++ ProjekatSIMS/ProjekatSIMS/Model/UpravnikModel/Prostorija.cs
@@ -1,87 +1,90 @@
-
 using Model.DoktorModel;
 using ProjekatSIMS.Model.UpravnikModel;
 using System;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace Model.UpravnikModel
 {
-   public class Prostorija
-   {
-      public Boolean Obrisi()
-      {
+    public class Prostorija
+    {
+        public Boolean Obrisi()
+        {
             // TODO: implement
             return true;
-      }
-   
-      public String id { get; set; }
-      public int sprat { get; set; }
-        public EnumProstorija vrsta { get; set; }
+        }
 
-        public System.Collections.ArrayList inventar;
-        public Prostorija()
+        public String id { get; set; }
+        public int sprat { get; set; }
+        public EnumProstorija vrsta { get; set; }
+        public List<Inventar> inventar;
+        Prostorija()
+        { }
+        Prostorija(String id, int sprat, EnumProstorija vrsta)
         {
+            this.id = id;
+            this.sprat = sprat;
+            this.vrsta = vrsta;
 
         }
-        public Prostorija(String id, int sprat, EnumProstorija vrsta, ArrayList inventar)
+        Prostorija(String id, int sprat, EnumProstorija vrsta, List<Inventar> inventar)
         {
             this.id = id;
             this.sprat = sprat;
             this.vrsta = vrsta;
             this.inventar = inventar;
+
         }
-        public Prostorija(String id, int sprat, EnumProstorija vrsta)
+        
+
+
+        public List<Inventar> GetInventar()
         {
-            this.id = id;
-            this.sprat = sprat;
-            this.vrsta = vrsta;
-            
+            if (inventar == null)
+                inventar = new List<Inventar>();
+            return inventar;
         }
 
 
-        public System.Collections.ArrayList GetInventar()
-      {
-         if (inventar == null)
-            inventar = new System.Collections.ArrayList();
-         return inventar;
-      }
-      
-      
-      public void SetInventar(System.Collections.ArrayList newInventar)
-      {
-         RemoveAllInventar();
-         foreach (Inventar oInventar in newInventar)
-            AddInventar(oInventar);
-      }
-      
-      
-      public void AddInventar(Inventar newInventar)
-      {
-         if (newInventar == null)
-            return;
-         if (this.inventar == null)
-            this.inventar = new System.Collections.ArrayList();
-         if (!this.inventar.Contains(newInventar))
-            this.inventar.Add(newInventar);
-      }
-      
-      
-      public void RemoveInventar(Inventar oldInventar)
-      {
-         if (oldInventar == null)
-            return;
-         if (this.inventar != null)
-            if (this.inventar.Contains(oldInventar))
-               this.inventar.Remove(oldInventar);
-      }
-      
-      
-      public void RemoveAllInventar()
-      {
-         if (inventar != null)
-            inventar.Clear();
-      }
-      public Pregled[] pregled;
-   
-   }
+        public void SetInventar(List<Inventar> newInventar)
+        {
+            RemoveAllInventar();
+            foreach (Inventar oInventar in newInventar)
+                AddInventar(oInventar);
+        }
+
+
+        public void AddInventar(Inventar newInventar)
+        {
+            if (newInventar == null)
+                return;
+            if (this.inventar == null)
+                this.inventar = new List<Inventar>();
+            if (!this.inventar.Contains(newInventar))
+                this.inventar.Add(newInventar);
+        }
+
+
+        public void RemoveInventar(Inventar oldInventar)
+        {
+            if (oldInventar == null)
+                return;
+            if (this.inventar != null)
+                if (this.inventar.Contains(oldInventar))
+                    this.inventar.Remove(oldInventar);
+        }
+
+
+        public void RemoveAllInventar()
+        {
+            if (inventar != null)
+                inventar.Clear();
+        }
+        public Pregled[] pregled;
+
+    
+
+
+
+
+    }
 }
