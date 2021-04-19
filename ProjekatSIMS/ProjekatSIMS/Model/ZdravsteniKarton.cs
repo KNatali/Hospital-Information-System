6b12d308d78a7f761ace64435b158ccb7b8613d6
KNatali <krsmanovic.in21.2018@uns.ac.rs>
2021-04-19 22:21:38 +0200

    Ispravljanje gresaka nakon mergovanja

--- ProjekatSIMS/ProjekatSIMS/Model/ZdravsteniKarton.cs
+++ ProjekatSIMS/ProjekatSIMS/Model/ZdravsteniKarton.cs
@@ -15,19 +15,7 @@ namespace Model
       
 
       /// <pdGenerated>default removeAll</pdGenerated>
-      public void RemoveAllAnamneza()
-      {
-         if (anamneza != null)
-         {
-            System.Collections.ArrayList tmpAnamneza = new System.Collections.ArrayList();
-            foreach (Anamneza oldAnamneza in anamneza)
-               tmpAnamneza.Add(oldAnamneza);
-            anamneza.Clear();
-            foreach (Anamneza oldAnamneza in tmpAnamneza)
-               oldAnamneza.SetZdravsteniKarton((ZdravsteniKarton)null);
-            tmpAnamneza.Clear();
-         }
-      }
+      
       public Pacijent pacijent { get; set; }
    
       public List<String> Alergeni { get; set; }
