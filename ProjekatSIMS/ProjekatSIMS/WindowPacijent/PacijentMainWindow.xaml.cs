398a824526b1361c985b66f94641848021dbb0bc
mbiljana <mbiljana99@gmail.com>
2021-05-23 23:20:07 +0200

    Dodato dugme na main window-u pacijenta

--- ProjekatSIMS/ProjekatSIMS/WindowPacijent/PacijentMainWindow.xaml.cs
+++ ProjekatSIMS/ProjekatSIMS/WindowPacijent/PacijentMainWindow.xaml.cs
@@ -77,5 +77,10 @@ namespace ProjekatSIMS.WindowPacijent
         {
             PacijentFrame.Content = new PregledPodsetnika();
         }
+
+        private void IzmeniPodsetnik(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
