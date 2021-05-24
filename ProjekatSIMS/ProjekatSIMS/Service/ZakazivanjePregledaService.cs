8b676fa8d9890ec5128611cf7dcbbce556c3d03e
mbiljana <mbiljana99@gmail.com>
2021-05-24 10:31:56 +0200

    Zakazivanje pregleda prozor sa informacijama

---
+++ ProjekatSIMS/ProjekatSIMS/Service/ZakazivanjePregledaService.cs
@@ -0,0 +1,13 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjekatSIMS.Service
+{
+    public class ZakazivanjePregledaService
+    {
+        public int MAKSIMALNO_OTKAZIVANJA = 10;
+        public PregledRepository pregledRepository = new PregledRepository();
+    }
+}
