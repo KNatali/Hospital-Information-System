c0bcbb77ee06ba18148ba038b801441c655c862d
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2021-06-01 10:31:33 +0200

    Dodati folderi za view i viewmodel

---
+++ ProjekatSIMS/ProjekatSIMS/ViewSekretar/Pocetna.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjekatSIMS.ViewSekretar
+{
+    /// <summary>
+    /// Interaction logic for Pocetna.xaml
+    /// </summary>
+    public partial class Pocetna : Window
+    {
+        public Pocetna()
+        {
+            InitializeComponent();
+        }
+    }
+}
