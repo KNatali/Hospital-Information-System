1dee9523a4509088cab69cd80ac4f0fac6d8ab8f
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2021-06-02 01:18:05 +0200

    Dijagram ne radi

--- ProjekatSIMS/ProjekatSIMS/ZauzetostProstorijaSWindow.xaml.cs
+++ ProjekatSIMS/ProjekatSIMS/ZauzetostProstorijaSWindow.xaml.cs
@@ -11,11 +11,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using LiveCharts;
 using LiveCharts.Wpf;
+using Model;
+using Separator = LiveCharts.Wpf.Separator;
 
 namespace ProjekatSIMS
 {
     public partial class ZauzetostProstorijaSWindow : Window
     {
+        public List<Prostorija> prostorije { get; set; }
+        public DateTime datumOd { get; set; }
+        public DateTime datumDo {get;set;}
         public SeriesCollection seriesCollection { get; set; }
         public string[] Datumi { get; set; }
         public Func<double,string> Zauzetost { get; set; }
@@ -28,16 +33,47 @@ namespace ProjekatSIMS
                 new ColumnSeries
                 {
                     Title="val1",
-                    Values=new ChartValues<double> {5,10}
+                    Values=new ChartValues<double> {0.1,0.4}
                 }
             };
             seriesCollection.Add(new ColumnSeries
             {
                 Title="val2",
-                Values=new ChartValues<double> {10,15}
+                Values=new ChartValues<double> {1,2}
             });
             Datumi = new[] { "values 1", "values 2" };
             Zauzetost = value => value.ToString("N"); 
+            /*Dijagram.Series.Add(new LineSeries
+            {
+                Values=new ChartValues<double> { 3,4,7,5,6,9 },
+                StrokeThickness=4,
+                StrokeDashArray=new System.Windows.Media.DoubleCollection(50),
+                PointGeometry=null
+            });
+            Dijagram.Series.Add(new LineSeries
+            {
+                Values = new ChartValues<double> { 6,2,4,1,3,5 },
+                StrokeThickness = 2,
+                PointGeometrySize = 20
+            });
+            Dijagram.AxisX.Add(new Axis
+            {
+                IsMerged=true,
+                Separator=new Separator
+                {
+                    StrokeThickness=1,
+                    StrokeDashArray=new DoubleCollection(2)
+                }
+            });
+            Dijagram.AxisY.Add(new Axis
+            {
+                IsMerged = true,
+                Separator = new Separator
+                {
+                    StrokeThickness = 1.5,
+                    StrokeDashArray = new DoubleCollection(5)
+                }
+            });*/
         }
 
         private void Stampaj(object sender, RoutedEventArgs e)
@@ -48,6 +84,18 @@ namespace ProjekatSIMS
         private void Nazad(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Prikazi(object sender, RoutedEventArgs e)
+        {
+            /*datumOd = (DateTime)DatumOD.SelectedDate;
+            datumDo = (DateTime)DatumDO.SelectedDate;
+            Dijagram.Visibility = Visibility.Visible;
+            Dijagram.Series.Clear();
+            seriesCollection = new SeriesCollection();
+
+            seriesCollection.Add(new LineSeries());
+            Dijagram.Series = seriesCollection;*/
         }
     }
 }
