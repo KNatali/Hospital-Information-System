50f0ca74d4f825fe15723427cf1dc01583f6c71b
mbiljana <mbiljana99@gmail.com>
2021-06-04 21:50:26 +0200

    Dodat View za terapiju

---
+++ ProjekatSIMS/ProjekatSIMS/ViewModelPacijent/TerapijaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ProjekatSIMS.ViewModelPacijent
+{
+   public  class TerapijaViewModel
+    {
+        public ObservableCollection<Recept> Recepti { get; set; }
+        public ReceptRepository receptRepository = new ReceptRepository();
+
+        public TerapijaViewModel()
+        {
+            LoadTerapija();
+        }
+
+        public void LoadTerapija()
+        {
+            ObservableCollection<Recept> recepti = new ObservableCollection<Recept>();
+
+        }
+    }
+}
