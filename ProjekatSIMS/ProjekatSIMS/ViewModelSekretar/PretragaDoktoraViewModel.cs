d83f6ec1ea4b89aeae848103ead5dfa22f66058b
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2021-06-05 10:59:15 +0200

    Pretraga doktora i pacijenata prelazak na mvvm

--- ProjekatSIMS/ProjekatSIMS/ViewModelSekretar/PretragaDoktoraViewModel.cs
+++ ProjekatSIMS/ProjekatSIMS/ViewModelSekretar/PretragaDoktoraViewModel.cs
@@ -1,16 +1,20 @@
-﻿using ProjekatSIMS.Commands;
+﻿using Model;
+using Newtonsoft.Json;
+using ProjekatSIMS.Commands;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Text;
 using System.Windows.Navigation;
 
 namespace ProjekatSIMS.ViewModelSekretar
 {
-    public class PretragaDoktoraViewModel
+    public class PretragaDoktoraViewModel : BindableBase
     {
-        public RelayCommand<string> NavCommand { get; private set; }
-        private BindableBase currentViewModel;
+        //public RelayCommand<string> NavCommand { get; private set; }
         private NavigationService navService;
+        public ObservableCollection<Doktor> Doktori { get; set; }
         public NavigationService NavService
         {
             get { return navService; }
@@ -22,6 +26,17 @@ namespace ProjekatSIMS.ViewModelSekretar
         public PretragaDoktoraViewModel(NavigationService service)
         {
             this.navService = service;
+            UcitajDoktore();
+        }
+        public void UcitajDoktore()
+        {
+            List<Doktor> doktori = new List<Doktor>();
+            using (StreamReader r = new StreamReader(@"..\..\..\Fajlovi\Doktor.txt"))
+            {
+                string json = r.ReadToEnd();
+                doktori = JsonConvert.DeserializeObject<List<Doktor>>(json);
+            }
+            Doktori = new ObservableCollection<Doktor>(doktori);
         }
     }
 }
